c99ebde8f0f4b7d52997eac1e35ad52e26b729db
BrianC4423 <1briancunningham1@gmail.com>
2015-03-25 03:48:10 -0400

    Delete test.rb

--- test.rb
+++
@@ -1,6 +0,0 @@
-line_width = 40
-book = ['table of contents', 'chapter 1', 'nature', 'page 2']
-
-puts (book[0].center(line_width))
-puts
-puts (book[1].ljust(line_width/3) + book[2].center(line_width/3) + book[3].ljust(line_width/3))
